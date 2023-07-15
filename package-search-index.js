6b369769211bd09f284106cf1bd251d49531741d
joshi1999 <joshi1999@yahoo.de>
2023-07-15 22:53:45 +0200

    Init Javadocs

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.joshi1999.pluginhelper.annotations"},{"l":"de.joshi1999.pluginhelper.bungeecord"},{"l":"de.joshi1999.pluginhelper.bungeecord.config"},{"l":"de.joshi1999.pluginhelper.utils"}];updateSearchResults();
